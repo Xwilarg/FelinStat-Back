407c9cbcd7b452de85b7870e4d9e1debe0b8f13f
Xwilarg <xwilarg@yahoo.fr>
2018-11-24 12:23:03 +0100

    Change default port to 8082
    8080 is used by default by RethinkDB

--- FelinStats-Back/Program.cs
+++ FelinStats-Back/Program.cs
@@ -20,7 +20,7 @@ namespace FelinStats_Back
             {
                 UrlReservations = new UrlReservations() { CreateAutomatically = true }
             };
-            NancyHost host = new NancyHost(config, new Uri("http://localhost:8080/"));
+            NancyHost host = new NancyHost(config, new Uri("http://localhost:8082/"));
             host.Start();
             Console.WriteLine("Host Started... Do ^C to exit.");
             Console.CancelKeyPress += (sender, e) =>
